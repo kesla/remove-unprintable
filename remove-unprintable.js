c0be7c48d79cb242b055744623ac1ad02c1d4a68
David Björklund <david.bjorklund@gmail.com>
2013-01-23 10:17:39 +0100

    [fix] Work correctly with \t

--- remove-unprintable.js
+++ remove-unprintable.js
@@ -1,10 +1,11 @@
 module.exports = function(str) {
-	var array = [];
-	for(var i = 0; i < str.length; ++i) {
-		var charCode = str.charCodeAt(i);
-		if (charCode >= 32) {
-			array.push(charCode);
-		}
-	}
-	return String.fromCharCode.apply(String, array);
+    var array = [];
+    for(var i = 0; i < str.length; ++i) {
+        var charCode = str.charCodeAt(i);
+        console.log(charCode);
+        if (charCode === 9 || charCode >= 32) {
+            array.push(charCode);
+        }
+    }
+    return String.fromCharCode.apply(String, array);
 }
