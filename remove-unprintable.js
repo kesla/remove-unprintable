8c0bf3e760a85b0f71e3a6956f2758070f0c3619
David Björklund <david.bjorklund@gmail.com>
2013-01-23 10:19:05 +0100

    [cleanup] remove debug statement

--- remove-unprintable.js
+++ remove-unprintable.js
@@ -2,7 +2,6 @@ module.exports = function(str) {
     var array = [];
     for(var i = 0; i < str.length; ++i) {
         var charCode = str.charCodeAt(i);
-        console.log(charCode);
         if (charCode === 9 || charCode >= 32) {
             array.push(charCode);
         }
