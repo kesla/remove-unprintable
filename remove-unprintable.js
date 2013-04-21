a22382e07079ac16370094045e556584da05bfb8
David Björklund <david.bjorklund@gmail.com>
2013-04-21 18:49:11 +0200

    [fix] Ignore char 65279 also

--- remove-unprintable.js
+++ remove-unprintable.js
@@ -2,7 +2,7 @@ module.exports = function(str) {
     var array = [];
     for(var i = 0; i < str.length; ++i) {
         var charCode = str.charCodeAt(i);
-        if (charCode === 9 || charCode >= 32) {
+        if (charCode === 9 || (charCode >= 32 && charCode !== 65279)) {
             array.push(charCode);
         }
     }
--- test/test.js
+++ test/test.js
@@ -6,5 +6,6 @@ test('remove unprintable chars', function(t) {
     t.equal(removeUnprintable('foo'), 'foo');
     t.equal(removeUnprintable('\u0000foo\u000b bar'), 'foo bar');
     t.equal(removeUnprintable('foo\tbar'), 'foo\tbar');
+    t.equal(removeUnprintable('foo' + String.fromCharCode(65279) + 'bar'), 'foobar');
     t.end();
 });
