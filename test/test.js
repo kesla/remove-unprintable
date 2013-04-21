64e44a675f190739c6b96e305f4e227d8e29098a
David Björklund <david.bjorklund@gmail.com>
2013-04-21 19:01:22 +0200

    [fix] keep \n

--- test/test.js
+++ test/test.js
@@ -7,5 +7,6 @@ test('remove unprintable chars', function(t) {
     t.equal(removeUnprintable('\u0000foo\u000b bar'), 'foo bar');
     t.equal(removeUnprintable('foo\tbar'), 'foo\tbar');
     t.equal(removeUnprintable('foo' + String.fromCharCode(65279) + 'bar'), 'foobar');
+    t.equal(removeUnprintable('foo\nbar'), 'foo\nbar');
     t.end();
 });
