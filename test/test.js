b54b949100ad5ff14eb7f47f34d900fe0e40bdc7
David Björklund <david.bjorklund@gmail.com>
2015-05-21 10:50:21 +0200

    use tape instead of tap for tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var test = require('tap').test;
+var test = require('tape');
 
 var removeUnprintable = require('../remove-unprintable');
 
@@ -7,5 +7,6 @@ test('remove unprintable chars', function(t) {
     t.equal(removeUnprintable('\u0000foo\u000b bar'), 'foo bar');
     t.equal(removeUnprintable('foo\tbar'), 'foo\tbar');
     t.equal(removeUnprintable('foo' + String.fromCharCode(65279) + 'bar'), 'foobar');
+    t.equal(removeUnprintable('foo\bbar'), 'foobar');
     t.end();
 });
