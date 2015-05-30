e3ec56a9b6f1822db140ef04c6229f580eb9dd53
David Björklund <david.bjorklund@gmail.com>
2015-05-31 00:58:58 +0200

    optimise for no-unprintable case

--- remove-unprintable.js
+++ remove-unprintable.js
@@ -1,10 +1,28 @@
 module.exports = function(str) {
+  if (!hasUnprintable(str)) {
+    return str;
+  }
+
   var array = [];
   for(var i = 0; i < str.length; ++i) {
     var charCode = str.charCodeAt(i);
-    if (charCode === 9 || (charCode >= 32 && charCode !== 65279)) {
+    if (!isUnprintable(charCode)) {
       array.push(charCode);
     }
   }
   return String.fromCharCode.apply(String, array);
+}
+
+function hasUnprintable (str) {
+  for(var i = 0; i < str.length; ++i) {
+    var charCode = str.charCodeAt(i);
+    if (isUnprintable(charCode)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isUnprintable (charCode) {
+  return charCode !== 9 && (charCode < 32 || charCode === 65279);
 }
