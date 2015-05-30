aa03aa1c917a9a685e65bacd110deda9773b6ec3
David Björklund <david.bjorklund@gmail.com>
2015-05-31 00:49:19 +0200

    2-spaces

--- remove-unprintable.js
+++ remove-unprintable.js
@@ -1,10 +1,10 @@
 module.exports = function(str) {
-    var array = [];
-    for(var i = 0; i < str.length; ++i) {
-        var charCode = str.charCodeAt(i);
-        if (charCode === 9 || (charCode >= 32 && charCode !== 65279)) {
-            array.push(charCode);
-        }
+  var array = [];
+  for(var i = 0; i < str.length; ++i) {
+    var charCode = str.charCodeAt(i);
+    if (charCode === 9 || (charCode >= 32 && charCode !== 65279)) {
+      array.push(charCode);
     }
-    return String.fromCharCode.apply(String, array);
+  }
+  return String.fromCharCode.apply(String, array);
 }
--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ var test = require('tape');
 var removeUnprintable = require('../remove-unprintable');
 
 test('remove unprintable chars', function(t) {
-    t.equal(removeUnprintable('foo'), 'foo');
-    t.equal(removeUnprintable('\u0000foo\u000b bar'), 'foo bar');
-    t.equal(removeUnprintable('foo\tbar'), 'foo\tbar');
-    t.equal(removeUnprintable('foo' + String.fromCharCode(65279) + 'bar'), 'foobar');
-    t.equal(removeUnprintable('foo\bbar'), 'foobar');
-    t.end();
+  t.equal(removeUnprintable('foo'), 'foo');
+  t.equal(removeUnprintable('\u0000foo\u000b bar'), 'foo bar');
+  t.equal(removeUnprintable('foo\tbar'), 'foo\tbar');
+  t.equal(removeUnprintable('foo' + String.fromCharCode(65279) + 'bar'), 'foobar');
+  t.equal(removeUnprintable('foo\bbar'), 'foobar');
+  t.end();
 });
